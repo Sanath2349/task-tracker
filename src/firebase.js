6d3fdb9889e32115c0cc3696445bf8fc91962f50
Sanath2349 <sanathredyg42@gmail.com>
2025-03-19 19:15:32 +0530

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0C4q-tNCEQIHYSFjUMTBrFmNmduWz4Dk",
-    authDomain: "task-manager-86cd9.firebaseapp.com",
-    projectId: "task-manager-86cd9",
-    storageBucket: "task-manager-86cd9.firebasestorage.app",
-    messagingSenderId: "716064278320",
-    appId: "1:716064278320:web:a3f0268288a5dbb3c3aaf5",
-    measurementId: "G-0FMZ1C1RJM"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 
 const app = initializeApp(firebaseConfig);
